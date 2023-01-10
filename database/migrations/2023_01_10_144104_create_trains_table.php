df6297724b1c7f8e081323665a20cd7a4b313337
Thunderbird93 <antircristian@gmail.com>
2023-01-10 15:50:42 +0100

    Modifica "up"

--- database/migrations/2023_01_10_144104_create_trains_table.php
+++ database/migrations/2023_01_10_144104_create_trains_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
