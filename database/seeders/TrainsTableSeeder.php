a7f613f74ad0865af4a448747c92aa67dc6e77c6
Thunderbird93 <antircristian@gmail.com>
2023-01-10 17:07:03 +0100

    work in progress punto 3

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Train;
+
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+    }
+}
